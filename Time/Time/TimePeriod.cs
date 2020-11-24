056078b69974cff10bd52ae61a100a91ab5e403f
Bartek-Szewczyk <ba20001000@gmail.com>
2020-11-24 16:43:28 +0100

    zmienne

---
+++ Time/Time/TimePeriod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Time
+{
+    struct TimePeriod
+    {
+        private Time time { get; }
+        private byte Hour { get; set; }
+        private byte Minutes { get; set; }
+        private byte Seconds { get; set; }
+    }
+}
