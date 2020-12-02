fb91f170b68ea6e3e5f3dab1c2542ccac4b5d2d9
Bartek-Szewczyk <ba20001000@gmail.com>
2020-12-02 22:36:49 +0100

    done

--- Time/Time/Program.cs
+++ Time/Time/Program.cs
@@ -10,22 +10,22 @@ namespace Time
     {
         static void Main()
         {
-            // Time t1 = new Time(12, 22, 09);
-            //Time t2 = new Time(1,2,3);
-            //Time t3 = new Time("12:32:11");
-            //TimePeriod tp1 = new TimePeriod(6352);
-            //TimePeriod tp2 = new TimePeriod("6:42:19");
+            Time t01 = new Time(12, 22, 09);
+            Time t02 = new Time(1, 2, 3);
+            Time t3 = new Time("12:32:11");
+            TimePeriod tp01 = new TimePeriod(6352);
+            TimePeriod tp02 = new TimePeriod("6:42:19");
 
 
-            //Time t1 = new Time(17, 31, 59);
-            //Time t2 = new Time(17, 31, 59);
-            //Console.WriteLine(t1);
+            Time t1 = new Time(17, 31, 59);
+            Time t2 = new Time(17, 31, 59);
+            Console.WriteLine(t1);
 
-            //TimePeriod tp1 = new TimePeriod("4:10:46");
+            TimePeriod tp03 = new TimePeriod("4:10:46");
 
-            //t1.Time_Plus(tp1);
+            t1.Time_Plus(tp01);
 
-            //Time tt1 = t2 + tp1;
+            Time tt1 = t2 + tp01;
             TimePeriod tp1 = new TimePeriod(35, 41, 28);
             TimePeriod tp2 = new TimePeriod("36:41:28");
             TimePeriod tp3 = new TimePeriod(73582);
